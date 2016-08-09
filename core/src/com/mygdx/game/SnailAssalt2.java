7363492c9f3bf1499f5bb5dc176907935757b7af
Ryanisaqt <ryan.shee@gmail.com>
2016-08-08 18:44:17 -0700

    Added game states and switch cases for them

--- core/src/com/mygdx/game/SnailAssalt2.java
+++ core/src/com/mygdx/game/SnailAssalt2.java
@@ -9,22 +9,64 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class SnailAssalt2 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	protected enum GameState {MAIN_MENU, LEVEL_SELECT, CHARACTER_SELECT, IN_GAME, GAME_OVER, STORE, SETTINGS}
+	protected static GameState state;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		reset();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		update();
+		draw();
+	}
+
+	public void reset () {
+		state = GameState.MAIN_MENU;
+	}
+
+	public void update() {
+		switch(state) {
+			case MAIN_MENU:
+				//code here
+				break;
+			case LEVEL_SELECT:
+				//code here
+				break;
+			case CHARACTER_SELECT:
+				//code here
+				break;
+			case IN_GAME:
+				//code here
+				break;
+			case GAME_OVER:
+				//code here
+				break;
+			case STORE:
+				//code here
+				break;
+			case SETTINGS:
+				//code here
+				break;
+			default:
+				//I don't think we need to put anything here
+				break;
+		}
+	}
+
+	public void draw () {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
