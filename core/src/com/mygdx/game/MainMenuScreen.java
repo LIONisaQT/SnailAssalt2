514fbf3f9f87a56757872916dbc194081b1eef2b
Ryan Shee <ryan.shee@gmail.com>
2016-08-10 01:28:59 -0700

    Deleted obsolete methods in main, began instance of font

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -16,6 +16,7 @@ public class MainMenuScreen implements Screen {
     public MainMenuScreen(final SnailAssalt2 gam) {
         game = gam;
         img = new Texture("badlogic.jpg");
+        game.state = SnailAssalt2.GameState.MAIN_MENU;
     }
 
 
@@ -23,6 +24,8 @@ public class MainMenuScreen implements Screen {
     public void show() {
         game.batch.begin();
         game.batch.draw(img, 0, 0);
+        game.font.getData().setScale(3);
+        game.font.draw(game.batch, "State: " + game.state, 10, Gdx.graphics.getHeight() - 10);
         game.batch.end();
     }
 
--- core/src/com/mygdx/game/SnailAssalt2.java
+++ core/src/com/mygdx/game/SnailAssalt2.java
@@ -1,62 +1,28 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SnailAssalt2 extends Game {
-	public SpriteBatch batch;
-    public BitmapFont font;
-
-	protected enum GameState {MAIN_MENU, LEVEL_SELECT, CHARACTER_SELECT, IN_GAME, GAME_OVER, STORE, SETTINGS}
-	protected static GameState state;
+	SpriteBatch batch;
+    BitmapFont font;
+    enum GameState {START, MAIN_MENU, LEVEL_SELECT, CHARACTER_SELECT, IN_GAME, GAME_OVER, STORE, SETTINGS}
+	GameState state;
 
 	protected static Preferences preferences;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+        font = new BitmapFont();
         preferences = new Preferences("Preferences");
-		reset();
+        this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
         super.render();
-	}
-
-	public void reset () {
-        this.setScreen(new MainMenuScreen(this));
-	}
-
-	public void update() throws IllegalStateException {
-		switch(state) {
-			case MAIN_MENU:
-				//code here
-				break;
-			case LEVEL_SELECT:
-				//code here
-				break;
-			case CHARACTER_SELECT:
-				//code here
-				break;
-			case IN_GAME:
-				//code here
-				break;
-			case GAME_OVER:
-				//code here
-				break;
-			case STORE:
-				//code here
-				break;
-			case SETTINGS:
-				//code here
-				break;
-			default:
-				throw new IllegalStateException("Somehow not in any of intended game states");
-		}
 	}
 
 	@Override
