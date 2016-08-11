83ec682c4e13ca8823a20136b3a3cb2a4f41f5bf
LIONisaQT <ryan.shee@gmail.com>
2016-08-10 19:30:57 -0700

    Added all empty screens, moved into new package

---
+++ core/src/com/mygdx/game/Screens/GameOverScreen.java
@@ -0,0 +1,57 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.mygdx.game.SnailAssalt2;
+
+/**
+ * Created by Ryan on 8/10/2016.
+ */
+public class GameOverScreen implements Screen {
+    final SnailAssalt2 game;
+
+    public GameOverScreen(final SnailAssalt2 gam) {
+        game = gam;
+    }
+
+    @Override
+    public void show() {
+        game.batch.begin();
+        game.font.getData().setScale(3);
+        game.font.draw(game.batch, "GAME_OVER", 10, Gdx.graphics.getHeight() - 10);
+        game.batch.end();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        show();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/mygdx/game/Screens/MainMenuScreen.java
@@ -0,0 +1,63 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.SnailAssalt2;
+
+/**
+ * Created by Ryan on 8/9/2016.
+ */
+public class MainMenuScreen implements Screen {
+    final SnailAssalt2 game;
+
+    Texture img;
+
+    public MainMenuScreen(final SnailAssalt2 gam) {
+        game = gam;
+        img = new Texture("badlogic.jpg");
+    }
+
+
+    @Override
+    public void show() {
+        game.batch.begin();
+        game.batch.draw(img, 0, 0);
+        game.font.getData().setScale(3);
+        game.font.draw(game.batch, "MAIN_MENU", 10, Gdx.graphics.getHeight() - 10);
+        game.batch.end();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        show();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
