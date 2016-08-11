8c235de23707c66300dbb71789c6e108c6bbb3e7
Lin <lchen00000@gmail.com>
2016-08-10 19:41:15 -0700

    scales to whatever resolution phones have

--- core/src/com/mygdx/game/SnailAssalt2.java
+++ core/src/com/mygdx/game/SnailAssalt2.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class SnailAssalt2 extends Game {
 	SpriteBatch batch;
@@ -13,16 +15,19 @@ public class SnailAssalt2 extends Game {
     float deltaTime;
     public static OrthographicCamera camera;
     private static Vector3 tap;
+	private Viewport viewport;
 
 	protected static Preferences preferences;
 
 	@Override
 	public void create () {
+        float width = Gdx.graphics.getWidth(), height = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera(width, height);
+        viewport = new FitViewport(1920, 1080, camera);
 		batch = new SpriteBatch();
         font = new BitmapFont();
         preferences = new Preferences("Preferences");
         deltaTime = Gdx.graphics.getDeltaTime();
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		tap = new Vector3();
         this.setScreen(new MainMenuScreen(this));
 	}
@@ -37,6 +42,11 @@ public class SnailAssalt2 extends Game {
 		batch.dispose();
         font.dispose();
 	}
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
 
 	public static Vector3 getTapPosition() { //gets and translates coordinates of tap to game world coordinates
 		tap.set(Gdx.input.getX(), Gdx.input.getY(), 0);
