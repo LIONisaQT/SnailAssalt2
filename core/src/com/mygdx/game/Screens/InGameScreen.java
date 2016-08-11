dd6e13d14062b62d3c34b0df616eaf31c82d8132
LIONisaQT <ryan.shee@gmail.com>
2016-08-10 20:14:58 -0700

    Added enemies to in-game state, can move from state to state

--- core/src/com/mygdx/game/Screens/InGameScreen.java
+++ core/src/com/mygdx/game/Screens/InGameScreen.java
@@ -3,7 +3,10 @@ package com.mygdx.game.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.mygdx.game.Enemy;
 import com.mygdx.game.SnailAssalt2;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ryan on 8/10/2016.
@@ -11,15 +14,25 @@ import com.mygdx.game.SnailAssalt2;
 public class InGameScreen implements Screen {
     final SnailAssalt2 game;
 
+    ArrayList<Enemy> enemies;
+
     public InGameScreen(final SnailAssalt2 gam) {
         game = gam;
+        enemies = new ArrayList<Enemy>();
+        for (int i = 0; i < 5; i++) {
+            enemies.add(new Enemy(0, (float)Math.random() * Gdx.graphics.getHeight()));
+        }
     }
 
     @Override
     public void show() {
         game.batch.begin();
+        for (Enemy e : enemies) {
+            e.draw(game.batch);
+        }
         game.font.getData().setScale(3);
         game.font.draw(game.batch, "IN_GAME", 10, Gdx.graphics.getHeight() - 10);
+        game.font.draw(game.batch, "Number of enemies: " + enemies.size(), 10, Gdx.graphics.getHeight() - 60);
         game.batch.end();
     }
 
@@ -27,6 +40,13 @@ public class InGameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        for (Enemy e : enemies) {
+            e.move();
+            if (e.getPosition().x >= Gdx.graphics.getWidth()) {
+                game.setScreen(new GameOverScreen(game));
+                dispose();
+            }
+        }
         show();
     }
 
